edb938fcfb4389d5aa98b79831221e884a52b3c4
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-09-26 21:15:42 -0300

    adding my branch

--- src/pages/Monera.js
+++ src/pages/Monera.js
@@ -1,15 +1,70 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React from "react";
+import { Text, SafeAreaView, View, ScrollView, Image } from "react-native";
+import styles from "../styles/global";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Monera  = () => {
+const Monera = () => {
+  // get -> lista info
+  // post -> cadastra nova ino
+  // delete -> deleta info
+  // put -> atualiza info
+
+  const [moneras, setMoneras] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("http://192.168.31.86:3000/monera")
+  //       const data = response.data
+  //       setMoneras(data)
+  //     } catch (error) {
+  //       console.error({error: 'Error fetching API'})
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.31.86:3000/monera")
+      .then((response) => {
+        setMoneras(response.data);
+      })
+      .catch(
+        (error) => console.log("Error", error)
+        // Alert.alert("Nao deu certo.")
+      );
+  }, []); //dispara uma unica vez
 
   return (
-    <SafeAreaView style={globalStyles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Tela do reino monera</Text>
+
+      <ScrollView>
+        {moneras.map((monera) => (
+          <View style={styles.contentContainer} key={monera.id}>
+            <Image
+              source={{ uri: monera.image }}
+              style={styles.contentImage}
+            ></Image>
+            <View style={styles.containerText}>
+              <Text style={styles.contentName}>{monera.name}</Text>
+              <Text style={styles.contentText}> {monera.description} </Text>
+              <Text style={styles.contentText}> {monera.nutrition} </Text>
+              <Text style={styles.contentText}> {monera.cellType} </Text>
+              <Text style={styles.contentText}>
+                {" "}
+                {monera.cellOrganization}{" "}
+              </Text>
+              <Text style={styles.contentText}> {monera.reproduction} </Text>
+              <Text style={styles.contentText}> {monera.respiration} </Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
 
 export default Monera;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,43 @@
 import { StyleSheet } from "react-native";
 
-export const globalStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#f0f0f0',
-      paddingVertical: 20,
-    }
-  });
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+    paddingVertical: 20,
+    backgroundColor: "#EFEFEF",
+  },
+  contentImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 6,
+    marginVertical: 15,
+    justifyContent: "center",
+  },
+  contentContainer: {
+    backgroundColor: "#FFF",
+    borderRadius: 6,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    elevation: 5,
+    marginTop: 20,
+    marginHorizontal: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 10,
+  },
+  containerText: {
+    alignItems: "flex-start",
+    width: "100%",
+    paddingStart: 5,
+    alignItems: "flex-start",
+  },
+  contentName: {
+    fontWeight: "bold",
+    marginBottom: 10,
+    fontSize: 19,
+  },
+  contentText: {
+    fontSize: 16,
+  },
+});
